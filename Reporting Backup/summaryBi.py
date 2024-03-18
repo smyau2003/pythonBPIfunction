996a2c7fa2aafb708b689a6cb8b8522d122ab550
Simon Yau <smsimonyau@gmail.com>
2024-03-18 09:17:52 +0800

    all good 180324am

--- Reporting Backup/summaryBi.py
+++ Reporting Backup/summaryBi.py
@@ -1,20 +1,23 @@
 import sys
-sys.path.append('{FUNCTION PATH}')
+sys.path.append('//192.168.0.209/share-editorial-research/Python Functions')
 
 
 import pandas as pa
 from PBI_function import  overall_calculations
 from openpyxl import load_workbook
 
-
-project_path = '{PROJECT PATH}'
+#demo data
+project_path = '//192.168.0.209/share-editorial-research/# Treasury Review/Treasury Review 2024/Contacts and Tally/Tally'    #can copy from windows path but remember to change \ to / and it needs to end with /
 ex_path= project_path + "/Excel/"
 tally_file = ex_path+'Tally.xlsx'
 rawdata_file = ex_path+'rawdata.xlsx'
+oldresult_file = ex_path+'oldresults.xlsx'
 
 tally=pa.read_excel(tally_file, "Tally", usecols=['Token',  'Valid', 'BankCode', 'Size', 'Region', 'Type'])  
 tally = tally[(tally['Valid']=='Y') | (tally['Valid']=='[Y]')]
 rawdata = pa.read_excel(rawdata_file,'data', skiprows=[1,2])
+
+
 
 field_list = pa.read_excel(rawdata_file, "fields", usecols=['code','section', 'type', 'field', 'question'])
 field_list = field_list.loc[field_list['type'] == 'Normal']  # using normal type fields, excluding banks 
@@ -34,7 +37,7 @@ for out in output:
     output[out] =  pa.merge(output[out], olddf, on=['AA-Question',  'AB-Answers'])
 
     
-
+print(output)
 
 
 
